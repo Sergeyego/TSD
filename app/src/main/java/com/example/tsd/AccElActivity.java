b997477aefd7ca318faa3f697f226f2c41c2c5ab
Sergeyego <sergen1@rambler.ru>
2022-02-21 13:29:48 +0300

    Inital

---
+++ app/src/main/java/com/example/tsd/AccElActivity.java
@@ -0,0 +1,14 @@
+package com.example.tsd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccElActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acc_el);
+    }
+}
