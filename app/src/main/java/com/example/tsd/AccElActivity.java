287bd61a7e12607aee9667c95586185cb441e3f9
Sergeyego <sergen1@rambler.ru>
2022-02-24 15:59:35 +0300

    прототип списка приемок

--- app/src/main/java/com/example/tsd/AccElActivity.java
+++ app/src/main/java/com/example/tsd/AccElActivity.java
@@ -1,14 +1,73 @@
 package com.example.tsd;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class AccElActivity extends AppCompatActivity {
+
+    Button btnUpd;
+    RecyclerView rv;
+
+    private List<RVAdapter.Acc> accs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acc_el);
+
+        btnUpd = (Button) findViewById(R.id.btnUpd);
+
+        accs = new ArrayList<>();
+
+        rv = (RecyclerView) findViewById(R.id.rvList);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+        //RVAdapter adapter = new RVAdapter(accs);
+        //rv.setAdapter(adapter);
+
+        btnUpd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeGetRequest();
+            }
+        });
+    }
+
+    private void makeGetRequest() {
+        new HttpReqGet().execute("http://192.168.1.10:3000/diam");
+    }
+
+    private class HttpReqGet extends HttpReq{
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            //Toast.makeText(getApplicationContext(),"resp: "+server_response, Toast.LENGTH_SHORT).show();
+
+            for (int i=0; i<10; i++){
+                RVAdapter.Acc a = new RVAdapter.Acc(String.valueOf(i),"Приемка из производства",new Date(),i);
+                accs.add(a);
+            }
+
+            RVAdapter.OnStateClickListener stateClickListener = new RVAdapter.OnStateClickListener() {
+                @Override
+                public void onStateClick(RVAdapter.Acc a, int position) {
+
+                    Toast.makeText(getApplicationContext(), "Был выбран пункт " + a.id,
+                            Toast.LENGTH_SHORT).show();
+                }
+            };
+
+            RVAdapter adapter = new RVAdapter(accs,stateClickListener);
+            rv.setAdapter(adapter);
+        }
     }
 }
---
+++ app/src/main/java/com/example/tsd/RVAdapter.java
@@ -0,0 +1,88 @@
+package com.example.tsd;
+
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Date;
+import java.util.List;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.AccViewHolder> {
+
+    public static class Acc {
+        String num;
+        String type;
+        Date dat;
+        int id;
+
+        Acc(String num, String type, Date dat, int id) {
+            this.num = num;
+            this.type = type;
+            this.dat = dat;
+            this.id = id;
+        }
+    }
+
+    List<Acc> list;
+
+    interface OnStateClickListener{
+        void onStateClick(Acc a, int position);
+    }
+    private final OnStateClickListener onClickListener;
+
+    RVAdapter(List<Acc> acc, OnStateClickListener onClickListener) {
+        this.list = acc;
+        this.onClickListener=onClickListener;
+    }
+
+    @NonNull
+    @Override
+    public AccViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.acc_view, parent, false);
+        AccViewHolder avh = new AccViewHolder(v);
+        return avh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AccViewHolder holder, int position) {
+        Acc a = list.get(position);
+        holder.num.setText(a.num);
+        holder.dat.setText(DateFormat.format("dd.MM.yy", a.dat).toString());
+        holder.type.setText(a.type);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                onClickListener.onStateClick(a, position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class AccViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView num;
+        TextView dat;
+        TextView type;
+
+        AccViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView) itemView.findViewById(R.id.cv);
+            num = (TextView) itemView.findViewById(R.id.acc_num);
+            dat = (TextView) itemView.findViewById(R.id.acc_dat);
+            type = (TextView) itemView.findViewById(R.id.acc_type);
+        }
+    }
+
+}
